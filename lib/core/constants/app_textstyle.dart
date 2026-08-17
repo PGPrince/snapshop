f2554dc3f1754cfead2a5803795e0be0456e8571
Prince <ghoshprince999@gmail.com>
2026-08-18 01:30:51 +0600

    add textstyle file

---
+++ lib/core/constants/app_textstyle.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class AppTextstyle {
+  // Inter Fonts
+  static const TextStyle interRegular = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle interMedium = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle interSemiBold = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle inteBold = TextStyle(
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w700,
+  );
+
+  // BeVietnamPro
+  static const TextStyle beVietnamProRegular = TextStyle(
+    fontFamily: 'BeVietnamPro',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle beVietnamProMedium = TextStyle(
+    fontFamily: 'BeVietnamPro',
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle beVietnamProSemiBold = TextStyle(
+    fontFamily: 'BeVietnamPro',
+    fontWeight: FontWeight.w600,
+  );
+
+  //Poppins
+  static const TextStyle poppinsRegular = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w400,
+  );
+
+  static const TextStyle poppinsMedium = TextStyle(
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w500,
+  );
+
+  //Urbanist
+  static const TextStyle urbanistBold = TextStyle(
+    fontFamily: 'Urbanist',
+    fontWeight: FontWeight.w700,
+  );
+}
