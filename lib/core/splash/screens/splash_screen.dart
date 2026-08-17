dcb8c704e100e7f5326b4242867202743542d33a
Prince <ghoshprince999@gmail.com>
2026-08-18 01:44:04 +0600

    add screen initialize screen utils and create splash screen

--- lib/core/splash/screens/splash_screen.dart
+++ lib/core/splash/screens/splash_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:snapshop/core/constants/app_color.dart';
+import 'package:snapshop/core/constants/app_textstyle.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -14,17 +16,26 @@ class SplashScreen extends StatelessWidget {
         children: [
           Center(
             child: Container(
-              width: 90,
-              height: 90,
+              width: 90.w,
+              height: 90.h,
               decoration: BoxDecoration(
                 color: AppColor.KBlack,
                 borderRadius: BorderRadius.circular(16),
               ),
               child: Image.asset(
                 'assets/images/splash/splash_logo.png',
-                width: 32,
-                height: 32,
+                width: 32.w,
+                height: 32.h,
               ),
+            ),
+          ),
+          SizedBox(height: 10.h),
+          Text(
+            'SnapShop',
+            style: AppTextstyle.inteBold.copyWith(
+              fontSize: 20.sp,
+              letterSpacing: 0.68.w,
+              height: 0.9,
             ),
           ),
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:snapshop/core/splash/screens/splash_screen.dart';
 
 void main() {
@@ -10,11 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: SplashScreen(),
+        );
+      },
     );
   }
 }
