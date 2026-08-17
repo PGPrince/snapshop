88833c2f4cecc9ae628fe9be16349c8d48c1cdf7
Prince <ghoshprince999@gmail.com>
2026-08-18 01:21:05 +0600

    add image with container

--- lib/core/splash/screens/splash_screen.dart
+++ lib/core/splash/screens/splash_screen.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:snapshop/core/constants/app_color.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.white);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              width: 90,
+              height: 90,
+              decoration: BoxDecoration(
+                color: AppColor.KBlack,
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Image.asset(
+                'assets/images/splash/splash_logo.png',
+                width: 32,
+                height: 32,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
