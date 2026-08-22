ab80285a8cf02798da37edd50a5ed0ef2a688fd3
Prince <ghoshprince999@gmail.com>
2026-08-22 21:34:00 +0600

    create category filter

---
+++ lib/core/common_widgets/category_filter/custom_category_filter.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class CustomCategoryFilter extends StatelessWidget {
+  final double? width;
+  final double? height;
+  final String title;
+  final TextStyle textStyle;
+  const CustomCategoryFilter({
+    super.key,
+    required this.title,
+    required this.textStyle,
+    this.width,
+    this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20.r),
+        border: Border.all(width: 1.w),
+      ),
+      child: Center(child: Text(title, style: textStyle)),
+    );
+  }
+}
