56a8ff1f24fda2d075b3d0ed336f32b904b092f5
Prince <ghoshprince999@gmail.com>
2026-08-22 21:17:09 +0600

    add search bar in home scree

--- lib/core/feature/home/screens/home_screen.dart
+++ lib/core/feature/home/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:snapshop/core/common_widgets/search_bar/custom_search_bar.dart';
+import 'package:snapshop/core/constants/app_colors.dart';
+import 'package:snapshop/core/constants/app_textstyle.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +14,32 @@ class HomeScreen extends StatefulWidget {
 class HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'SnapShop',
+          style: AppTextstyle.interBold.copyWith(fontSize: 24.sp, height: 0.75),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: CustomSearchBar(
+              width: 343.w,
+              height: 52.h,
+              hintText: 'Search',
+              prefixIcon: Icons.search,
+              iconColor: AppColors.kBlack,
+              suffixIcons: [
+                Icons.camera_alt_outlined,
+
+                Icons.qr_code_scanner_outlined,
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
