222c8b9f59daae90be681520f675cf586798bb24
Prince <ghoshprince999@gmail.com>
2026-08-28 21:55:56 +0600

    create product card

---
+++ lib/core/cards/product_card.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:snapshop/core/constants/app_colors.dart';
+
+class ProductCard extends StatelessWidget {
+  final String path;
+  final String title;
+  final String subTitle;
+  final TextStyle titleStyle;
+  final TextStyle subTitleStyle;
+  const ProductCard({
+    super.key,
+    required this.path,
+    required this.title,
+    required this.subTitle,
+    required this.titleStyle,
+    required this.subTitleStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 163.w,
+      height: 217.h,
+      child: Stack(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(16.r),
+            child: Image.asset(path, width: 163.w, height: 217.h),
+          ),
+
+          Positioned(
+            right: 12,
+            top: 12,
+            child: Container(
+              width: 30.w,
+              height: 30.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15.r),
+                color: AppColors.kBlack,
+              ),
+              child: Icon(
+                Icons.favorite_outline,
+                color: AppColors.kWhite,
+                size: 16,
+              ),
+            ),
+          ),
+
+          Positioned(
+            left: 12,
+            bottom: 10,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: titleStyle,
+                  // style: titleStyle
+                ),
+                Text(
+                  subTitle,
+                  style: subTitleStyle,
+                  // style: subTitleStyle
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            right: 12,
+            bottom: 12,
+            child: Container(
+              width: 30.w,
+              height: 30.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: AppColors.kBlack,
+              ),
+              child: Icon(
+                Icons.shopping_bag_outlined,
+                color: AppColors.kWhite,
+                size: 18,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
