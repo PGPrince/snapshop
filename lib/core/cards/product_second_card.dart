13386e5adee14184f5de758ab2833f8dcfd4eb35
Prince <ghoshprince999@gmail.com>
2026-08-29 11:55:06 +0600

    add product second card

---
+++ lib/core/cards/product_second_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:snapshop/core/constants/app_colors.dart';
+
+class ProductSecondCard extends StatelessWidget {
+  final String path;
+  // final String title;
+  // final String subTitle;
+  // final TextStyle titleStyle;
+  // final TextStyle subTitleStyle;
+  const ProductSecondCard({
+    super.key,
+    required this.path,
+    // required this.title,
+    // required this.subTitle,
+    // required this.titleStyle,
+    // required this.subTitleStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 161.w,
+      height: 208.h,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.amber,
+      ),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Positioned(
+            top: 20,
+            left: 20,
+            right: 20,
+
+            child: Image.asset(path, width: 96.w, height: 96.h),
+          ),
+
+          Positioned(
+            right: 12,
+            top: 12,
+            child: Icon(
+              Icons.favorite_outline,
+              color: AppColors.kBlack,
+              size: 22,
+            ),
+          ),
+
+          Positioned(
+            top: 115,
+            left: 30,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [Text("Bentwood Chair"), Text('\$68.00')],
+            ),
+          ),
+
+          Positioned(
+            left: 61,
+            bottom: -18,
+            child: Container(
+              width: 40.w,
+              height: 40.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(42),
+                color: AppColors.kBlack,
+              ),
+              child: Icon(
+                Icons.shopping_bag_outlined,
+                color: AppColors.kWhite,
+                size: 22,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
