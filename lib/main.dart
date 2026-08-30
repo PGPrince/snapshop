56ea4378db3318f21f487d22c5b6fe5580a3e18b
Prince <ghoshprince999@gmail.com>
2026-08-30 13:30:27 +0600

    create Cart screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:snapshop/core/feature/home/screens/product_details_screen.dart';
+import 'package:snapshop/core/feature/cart_and_checkout/cart_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             colorScheme: .fromSeed(seedColor: Colors.deepPurple),
           ),
-          home: ProductDetailsScreen(),
+          home: CartScreen(),
         );
       },
     );
