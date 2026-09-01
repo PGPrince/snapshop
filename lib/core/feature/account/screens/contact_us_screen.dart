2ff6df4e5976a3c85cee6d5b5219cad531f5cdbd
Prince <ghoshprince999@gmail.com>
2026-09-01 20:35:01 +0600

    create contact us screen

---
+++ lib/core/feature/account/screens/contact_us_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:snapshop/core/common_widgets/buttons/custom_icon_button.dart';
+import 'package:snapshop/core/constants/app_colors.dart';
+import 'package:snapshop/core/constants/app_textstyle.dart';
+
+class ContactUsScreen extends StatelessWidget {
+  const ContactUsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.kWhite,
+        leading: CustomIconButton(
+          icon: Icons.arrow_back,
+          color: AppColors.kBlack,
+        ),
+        title: Center(
+          child: Text(
+            'Contact Us',
+            style: AppTextstyle.interBold.copyWith(
+              fontSize: 24.sp,
+              height: 1.3,
+              letterSpacing: -0.48,
+            ),
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(12.r),
+        child: Column(
+          children: [
+            Container(
+              width: 343.w,
+              height: 72.h,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16.0),
+                color: AppColors.kWhite,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(16.r),
+                child: Row(
+                  children: [
+                    Icon(Icons.headset_mic),
+                    SizedBox(width: 10.h),
+                    Text(
+                      'Customer Service',
+                      style: AppTextstyle.interSemiBold.copyWith(
+                        fontSize: 18.sp,
+                        height: 1.4,
+                        letterSpacing: 0.3,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
