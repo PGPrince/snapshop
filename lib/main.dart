7a42f485c86d7b5d8a7b00d92cfb87884c5da03d
Prince <ghoshprince999@gmail.com>
2026-09-01 15:43:52 +0600

    add google map api

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:snapshop/core/feature/cart_and_checkout/cart_screen.dart';
-import 'package:snapshop/core/feature/cart_and_checkout/order_successful_screen.dart';
+import 'package:snapshop/core/feature/cart_and_checkout/add_new_address_screen.dart';
+import 'package:snapshop/core/feature/cart_and_checkout/checkout_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             colorScheme: .fromSeed(seedColor: Colors.deepPurple),
           ),
-          home: OrderSuccessfulScreen(),
+          home: AddNewAddressScreen(),
         );
       },
     );
