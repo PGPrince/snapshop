619b1cb53c1fff60dc1163c45c8a11b7740e4165
Prince <ghoshprince999@gmail.com>
2026-09-01 20:27:45 +0600

    create terms & conditions screen

--- lib/core/feature/account/screens/terms_and_conditions_screen.dart
+++ lib/core/feature/account/screens/terms_and_conditions_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:snapshop/core/common_widgets/buttons/custom_icon_button.dart';
