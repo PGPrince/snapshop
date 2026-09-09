63a19d18fbbcf02ced0b7433b9377dbcb245ea41
Prince <ghoshprince999@gmail.com>
2026-09-10 01:38:38 +0600

    remove center widget and add centerTile in appbar

--- lib/core/feature/cart_and_checkout/screens/cart_screen.dart
+++ lib/core/feature/cart_and_checkout/screens/cart_screen.dart
@@ -27,14 +27,13 @@ class _CartScreenState extends State<CartScreen> {
           icon: Icons.arrow_back,
           color: AppColors.kBlack,
         ),
-        title: Center(
-          child: Text(
-            'Cart',
-            style: AppTextstyle.interBold.copyWith(
-              fontSize: 24.sp,
-              height: 1.3,
-              letterSpacing: -0.48,
-            ),
+        centerTitle: true,
+        title: Text(
+          'Cart',
+          style: AppTextstyle.interBold.copyWith(
+            fontSize: 24.sp,
+            height: 1.3,
+            letterSpacing: -0.48,
           ),
         ),
       ),
