08e815fe27e8839d8905fe8b18ae4427b7230a8e
Prince <ghoshprince999@gmail.com>
2026-09-10 01:31:34 +0600

    remove center widget and add centerTile in appbar

--- lib/core/feature/account/screens/contact_us_screen.dart
+++ lib/core/feature/account/screens/contact_us_screen.dart
@@ -16,14 +16,13 @@ class ContactUsScreen extends StatelessWidget {
           icon: Icons.arrow_back,
           color: AppColors.kBlack,
         ),
-        title: Center(
-          child: Text(
-            'Contact Us',
-            style: AppTextstyle.interBold.copyWith(
-              fontSize: 24.sp,
-              height: 1.3,
-              letterSpacing: -0.48,
-            ),
+        centerTitle: true,
+        title: Text(
+          'Contact Us',
+          style: AppTextstyle.interBold.copyWith(
+            fontSize: 24.sp,
+            height: 1.3,
+            letterSpacing: -0.48,
           ),
         ),
       ),
