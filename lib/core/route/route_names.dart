a78b38922b1d8689f502d866e32693fede545f16
Prince <ghoshprince999@gmail.com>
2026-09-15 00:11:03 +0600

    define named routes for app screens

---
+++ lib/core/route/route_names.dart
@@ -0,0 +1,52 @@
+class RouteNames {
+  // Splash Screen
+  static const String splashScreen = 'SplashScreen';
+
+  // Onboarding
+  static const String onboardingScreen = 'OnboardingScreen';
+
+  // Auth
+  static const String loginScreen = 'LoginScreen';
+  static const String signUpScreen = 'SignUpScreen';
+  static const String verifyPhoneNumberScreen = 'VerifyPhoneNumberScreen';
+  static const String forgotPasswordScreen = 'ForgotPasswordScreen';
+  static const String newPasswordScreen = 'NewPasswordScreen';
+  static const String successScreen = 'SuccessScreen';
+
+  // Home
+  static const String homeScreen = 'HomeScreen';
+  static const String searchScreen = 'SearchScreen';
+  static const String customNavigationBar = 'CustomNavigationBar';
+  static const String categoriesScreen = 'CategoriesScreen';
+  static const String productDetailsScreen = 'ProductDetailsScreen';
+
+  // Cart
+  static const String cartScreen = 'CartScreen';
+  static const String checkoutScreen = 'CheckoutScreen';
+  static const String addNewAddressScreen = 'AddNewAddressScreen';
+  static const String orderSuccessfulScreen = 'OrderSuccessfulScreen';
+
+  // WishList
+  static const String wishlistScreen = 'WishlistScreen';
+
+  // Account
+  static const String myAccountScreen = 'MyAccountScreen';
+  static const String accountInfoScreen = 'AccountInfoScreen';
+  static const String paymentMethodScreen = 'PaymentMethodScreen';
+  static const String addNewCardScreen = 'AddNewCardScreen';
+  static const String scanNewCardScreen = 'ScanNewCardScreen';
+  static const String deliveryAddressScreen = 'DeliveryAddressScreen';
+  static const String addNewAddressScreenFirst = 'AddNewAddressScreenFirst';
+  static const String addNewAddressScreenSecond = 'AddNewAddressScreenSecond';
+  static const String changePasswordScreen = 'ChangePasswordScreen';
+  static const String notificationsScreen = 'NotificationsScreen';
+  static const String languageScreen = 'LanguageScreen';
+  static const String helpCenterScreen = 'HelpCenterScreen';
+  static const String termsAndConditionsScreen = 'TermsAndConditionsScreen';
+  static const String contactUsScreen = 'ContactUsScreen';
+
+  // Order
+  static const String ordersScreen = 'OrdersScreen';
+  static const String orderDetailsScreen = 'OrderDetailsScreen';
+  static const String trackOrderScreen = 'TrackOrderScreen';
+}
